eb716a0186f71a13a28de8fbc3feae75d928e0b9
Ith Chanakijkamjorn <ithcha2525@gmail.com>
2026-08-12 13:38:56 +0700

    TEST

--- src/app/products/_data/productTypes.js
+++ src/app/products/_data/productTypes.js
@@ -173,7 +173,7 @@ export const productTypes = [
     slug: 'optopus',
     name: 'Optopus',
     description: 'Advanced fibre optic and IP solutions for modern network infrastructures.',
-    color: '#00385E',
+    color: '#ffffff',
     image: "/images/products/optopus.png",
 
     features: [
